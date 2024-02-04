09ba26c075907d4593ad7de0c823fbbf1fa826b5
Martin Erpicum <martin@erpicum.net>
2024-02-04 11:43:34 +0100

    Update MapComponent.vue

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -4,13 +4,16 @@
     style="height: 300px"
     :zoom="zoom"
     :center="center"
+    :options="{attributionControl: false}"
   >
     <l-tile-layer
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       layer-type="base"
       name="OpenStreetMap"
-      credit="OpenStreetMap"
-      attribution="© Contributors"
+    />
+    <l-control-attribution
+      position="topright"
+      prefix="A custom prefix"
     />
     <div
       v-for="marche in marches"
@@ -29,14 +32,14 @@
   </l-map>
 
   <MarcheDescription
-    v-if="selected_map"
-    :marche="selected_map"
+    v-if="selected_marche"
+    :marche="selected_marche"
   />
 </template>
 
 <script>
 import 'leaflet/dist/leaflet.css';
-import { LMap, LTileLayer, LMarker, LTooltip } from '@vue-leaflet/vue-leaflet';
+import { LMap, LTileLayer, LMarker, LTooltip, LControlAttribution} from '@vue-leaflet/vue-leaflet';
 import MarcheDescription from '@/components/partials/MarcheDescription.vue';
 
 export default {
@@ -45,24 +48,36 @@ export default {
     LTileLayer,
     LMarker,
     LTooltip,
-    MarcheDescription
+    LControlAttribution,
+    MarcheDescription,
   },
   props: {
     marches: {
       type: Object,
       required: true,
     },
+    marcheFromList: {
+      type: Object,
+      required: false,
+      default: null,
+    },
   },
   data() {
     return {
       zoom: 8,
       center: [50.8503, 4.3517],
-      selected_map: null,
+      selected_marche: null,
     };
+  },
+  mounted() {
+    if (this.marcheFromList) {
+      this.selected_marche = this.marcheFromList;
+      this.center = this.marcheFromList.latLong;
+    }
   },
   methods: {
     openDetails(marche) {
-      this.selected_map = marche;
+      this.selected_marche = marche;
       this.center = marche.latLong;
     },
   },
